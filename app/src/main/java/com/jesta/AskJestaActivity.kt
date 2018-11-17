50dc23f6b68a35dddb9c3c00c8146e727eb7441f
max <maxim-chi@campus.technion.ac.il>
2018-11-17 18:48:00 +0200

    adding two new activities

---
+++ app/src/main/java/com/jesta/AskJestaActivity.kt
@@ -0,0 +1,12 @@
+package com.jesta
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AskJestaActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ask_jesta)
+    }
+}
