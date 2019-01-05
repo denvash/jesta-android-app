8cbf1e5a4417bff5725df5ec73fee81afd18adf4
denvash <justdenva@gmail.com>
2019-01-05 18:09:09 +0200

    [~] Status change

--- app/src/main/java/com/jesta/data/Status.kt
+++ app/src/main/java/com/jesta/data/Status.kt
@@ -1,7 +1,7 @@
 package com.jesta.data
 
 data class Status(
-    val missionID: List<String>,
+    val missionID: String,
     val doerIDList: List<String>,
     val isPoster: Boolean,
     val status: String
